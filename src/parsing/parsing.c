3b957f024b9f4cf241c270b4c19cd44d9da5393b
Thomas Gillain <thgillai@fu-r10-p2.s19.be>
2021-10-19 20:12:33 +0200

    small fixes

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: thgillai <thgillai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/13 12:19:53 by thgillai          #+#    #+#             */
-/*   Updated: 2021/10/19 15:25:51 by thgillai         ###   ########.fr       */
+/*   Updated: 2021/10/19 20:05:27 by thgillai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,10 @@ void	parsing3(t_data *data, char *line)
 	}
 	else if (line[0] == 0)
 		return ;
+	if (line[0] != 'N' && line[0] != 'S' && line[0] != 'W' && line[0] != 'E'
+		&& line[0] != 'C' && line[0] != 'F' && line[0] != '1'
+		&& line[0] != '0' && line[0] != ' ')
+		exit_error("111111");
 }
 
 void	parsing2(t_data *data, char *line)
